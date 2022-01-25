2f1f544d7598edbea9677f43d3063f315863950c
CrypNote <97964987+CrypNoteio@users.noreply.github.com>
2022-01-25 18:02:58 +0800

    Update NoteNFT.sol

--- NoteNFT.sol
+++ NoteNFT.sol
@@ -20,6 +20,7 @@ contract NoteNFT is ERC721Enumerable, Ownable {
     EnumerableSet.AddressSet private whitelist;
     mapping(address => bool) public whiteMintedMap;
 
+ 
     constructor(uint256 _wlStartTime, uint256 _publicStartTime, address _creator) ERC721("Note NFT", "NOTE") {
         wlStartTime = _wlStartTime;
         publicStartTime = _publicStartTime;
@@ -27,7 +28,6 @@ contract NoteNFT is ERC721Enumerable, Ownable {
     }
 
     function addWhitelist(address[] memory _whitelist) external onlyOwner {
-        require(whitelist.length() + _whitelist.length <= WhitelistMaxNumber, "Exceed whitelist max number");
         for (uint i = 0; i < _whitelist.length; i++) {
             if (!whitelist.contains(_whitelist[i])) {
                 whitelist.add(_whitelist[i]);
@@ -35,18 +35,42 @@ contract NoteNFT is ERC721Enumerable, Ownable {
         }
     }
 
+    function whitelistLength() external view returns(uint256) {
+        return whitelist.length();
+    }
+
+    function isExistInWhitelist(address _userAddr) external view returns(bool) {
+        return whitelist.contains(_userAddr);
+    }
+
+    function getAllWhitelist() external view returns(address[] memory wlAddrs) {
+        uint256 length = whitelist.length();
+        wlAddrs = new address[](length);
+        for (uint256 i = 0; i < length; i++) {
+            wlAddrs[i] = whitelist.at(i);
+        }
+    }
+
+    function setPublicTime(uint256 _publicStartTime) external onlyOwner {
+        require(_publicStartTime > wlStartTime, "Public sale time should be larger.");
+        publicStartTime = _publicStartTime;
+    }
+
     function mint() payable external {
         require(msg.value == mintPrice, "Mint price is 0.05 ETH");
         require(tokenId < MaxSupply, "Exceed max supply 1024");
         require(block.timestamp >= wlStartTime, "NOT start");
+        require(!whiteMintedMap[msg.sender], "One account can NOT mint more than once");
         if (block.timestamp < publicStartTime) {
             require(whitelist.contains(msg.sender), "NOT in whitelist");
-            require(!whiteMintedMap[msg.sender], "can NOT mint more than once");
+            require(tokenId < WhitelistMaxNumber, "Exceed wl supply 124");
             tokenId++;
-            _mint(msg.sender, tokenId);
             whiteMintedMap[msg.sender] = true;
+            _mint(msg.sender, tokenId);
             return;
         } 
+        
+        whiteMintedMap[msg.sender] = true;
         tokenId++;
         _mint(msg.sender, tokenId);
     }
